e591fe39b2a4d3251a7c759d7a9bc5779b7a1200
Will Tran <wtran@pivotal.io>
2015-10-20 23:58:38 -0400

    Adding log message

--- hello-service-eureka/src/main/java/demo/HelloServiceEureka.java
+++ hello-service-eureka/src/main/java/demo/HelloServiceEureka.java
@@ -1,5 +1,7 @@
 package demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableDiscoveryClient
 public class HelloServiceEureka {
 
+	private static final Logger log = LoggerFactory.getLogger(HelloServiceEureka.class);
+
     public static void main(String[] args) {
         SpringApplication.run(HelloServiceEureka.class, args);
     }
@@ -27,6 +31,7 @@ public class HelloServiceEureka {
 
     @RequestMapping("/")
     public String hello() {
+		log.info("saying hello");
         return helloProperties().getMessage();
     }
 
