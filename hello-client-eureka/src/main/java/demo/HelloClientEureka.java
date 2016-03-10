69c35194b3d0da690acba2ecf61fecf38c43815a
Will Tran <wtran@pivotal.io>
2016-03-10 15:05:34 -0600

    updating default SC version to Brixton.BUILD-SNAPSHOT
    Explicit creation of a @LoadBalanced RestTemplate seems to be necessary
    now.

--- hello-client-eureka/src/main/java/demo/HelloClientEureka.java
+++ hello-client-eureka/src/main/java/demo/HelloClientEureka.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +27,12 @@ public class HelloClientEureka {
     @RequestMapping("/")
     public String hello() {
         return helloService.getGreeting();
+    }
+    
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+    	return new RestTemplate();
     }
 
     @Component
--- hello-client-hystrix/src/main/java/demo/HelloClientHystrix.java
+++ hello-client-hystrix/src/main/java/demo/HelloClientHystrix.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +31,12 @@ public class HelloClientHystrix {
     @RequestMapping("/")
     public String hello() {
         return helloService.getGreeting();
+    }
+    
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+    	return new RestTemplate();
     }
 
     @Component
