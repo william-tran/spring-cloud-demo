cd88fdb8f76a2868dbbbb571adebde59bf1ba9d3
William Tran <wtran@wtran.af.pivotal.io>
2016-06-15 20:46:43 +0000

    Revert "nope, that didn't work. I think I'll need a separate branch for"
    This reverts commit ba56b5536c954e9d7c9497c7c684d412962c1dd4.

--- hello-client-hystrix/src/main/java/demo/HelloClientHystrix.java
+++ hello-client-hystrix/src/main/java/demo/HelloClientHystrix.java
@@ -6,10 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
@@ -31,6 +34,12 @@ public class HelloClientHystrix {
         return helloService.getGreeting();
     }
     
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+    	return new RestTemplate();
+    }
+
     @Component
     public static class HelloService {
 
